89cb4334783f4fc291f76ea5d385706be6dce1c0
carinaramos <cgramos50@gmail.com>
2021-06-09 14:28:12 -0500

    dialogs

--- client/src/Cards.js
+++ client/src/Cards.js
@@ -8,6 +8,7 @@ import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+import TutorDialog from './TutorDialog'
 // import { useStyles } from './useStyles';
 
 const useStyles = makeStyles((theme) => ({
@@ -31,13 +32,26 @@ const useStyles = makeStyles((theme) => ({
 
 export default function Cards(props){
   const tutors = props.tutorList;
+  const [currentTutor, setCurrentTutor] = React.useState({});
+  const [dialogOpen, setDialogOpen] = React.useState(false);
   const classes = useStyles();
-  
+
+  const handleDialogOpen = (tutor) => {
+    console.log(tutor);
+    setCurrentTutor(tutor);
+    setDialogOpen(true);
+  };
+
+  const handleDialogClose = () => {
+    setDialogOpen(false);
+  };
+
   return (
-    <Container 
+    <Container
       className={classes.cardGrid} 
       maxWidth="md"
       >
+      <TutorDialog tutor={currentTutor} open={dialogOpen} onClose={handleDialogClose} />
       <Grid container spacing={4}>
         {tutors.map((tutor) => (
           <Grid item key={tutor._id} xs={12} sm={6} md={4}>
@@ -52,15 +66,15 @@ export default function Cards(props){
                   className={classes.cardContent}
                   >
                 <Typography gutterBottom variant="h5" component="h2">
-                  {tutor.firstName} {tutor.lastName}
+                  {tutor.firstName} {tutor.lastName[0]}.
                 </Typography>
                 <Typography paragraph>
                   {tutor.areas}
                 </Typography>
               </CardContent>
               <CardActions>
-                <Button size="small" color="primary">
-                  View
+                <Button size="small" color="primary" onClick={() => handleDialogOpen(tutor)}>
+                  Read more
                 </Button>
               </CardActions>
             </Card>
