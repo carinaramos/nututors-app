16f90d5837fc84b6d4013eb819f2d22820446214
carinaramos <cgramos50@gmail.com>
2021-06-10 09:40:32 -0500

    relaunch

--- client/src/Cards.js
+++ client/src/Cards.js
@@ -9,7 +9,6 @@ import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import TutorDialog from './TutorDialog'
-// import { useStyles } from './useStyles';
 
 const useStyles = makeStyles((theme) => ({
 
